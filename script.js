09e4e9c65ffa14896fe6f512477a4e5ec0790aab
haralmeghana <128888658+haralmeghana@users.noreply.github.com>
2023-06-10 19:59:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+// Songs =[
+//     {
+//         name:"A-Thousand-Years-Christina",
+//         title:"A Thousand Years",
+//         artist:"Christina Perri",
+//     },
+//     {
+//         name:"Calm-Down-Calm-Down",
+//         title:"Calm Down",
+//         artist:"Rema",
+//     },
+//     {
+//         name:"La-La-La",
+//         title:"La La La",
+//         artist:"Shakira",
+//     },
+//     {
+//         name:"I-Like-Me-Better",
+//         title:"I Like Me Better",
+//         artist:"Lauv",
+//     },
+//     {
+//         name:"Hips_Dont_Lie",
+//         title:"Hips Don't Lie",
+//         artist:"Shakira",
+//     },
+//     {
+//         name:"Unholy",
+//         title:"Unholy",
+//         artist:"Sam Smith",
+        
+//     }
+// ]
